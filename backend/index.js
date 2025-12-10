08674144be47a95d79c5fcec31a87f640d15795a
Rohithgowdak18 <rohithgowdak18@gmail.com>
2025-12-10 15:33:01 +0530

    done

--- backend/index.js
+++ backend/index.js
@@ -12,14 +12,15 @@ app.use(express.json());
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(bodyParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 const todoItemsRoute = require('./routes/todoItems')
 
 
 
-mongoose.connect(process.env.DB_CONNECT)
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Database Connected'))
     .catch(err => console.log(err));
 
